0ba538834761ea8d2529383cc4004b0da7e41ad4
machettie <55490804+machettie@users.noreply.github.com>
2020-10-16 20:25:04 +0400

    замечание исправлены теперь все элементы кладутся в одну коллекцию добавлен метод для удаления лишних элементов добавлен метод для красивого вывода температуры на экран

--- src/main/java/ru/dexsys/App.java
+++ src/main/java/ru/dexsys/App.java
@@ -1,14 +1,12 @@
 package ru.dexsys;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App
 {
     public static void main( String[] args ) {
-        WebDriver driver = new ChromeDriver();
-        WeatherPage page = new WeatherPage(driver);
-        page.getTempTenDays();
+        WeatherPage page = new WeatherPage(new ChromeDriver());
+        page.getWeekTemp();
         page.exit();
     }
 }
--- src/main/java/ru/dexsys/WeatherPage.java
+++ src/main/java/ru/dexsys/WeatherPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import javax.swing.*;
 import java.util.List;
 
 public class WeatherPage {
@@ -14,22 +15,43 @@ public class WeatherPage {
     public WeatherPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         webDriver.get(URL);
-        PageFactory.initElements(webDriver,this);
+        PageFactory.initElements(webDriver, this);
     }
 
-    public void getTempTenDays() {
-        List<WebElement> dayNumber = webDriver.findElements(By.xpath("//strong"));
-        List<WebElement> monthAndDayOfWeek = webDriver.findElements(By.xpath("//small"));
-        List<WebElement> tempDayPart = webDriver.findElements(By.className("weather-table__body-cell_type_daypart"));
-        int j=0;
-        for(int i=0; i < tempDayPart.size();i++) {
-            if(i % 4 == 0) {
-                System.out.print(dayNumber.get(j).getText());
-                System.out.println(monthAndDayOfWeek.get(j).getText());
-                j++;
-            }
-            System.out.println(tempDayPart.get(i).getText());
+    // Так как на сайте прогноз погоды на 10 дней, а нам нужно 7 - удаляем лишнее
+    private void removeExtra(List<WebElement> removeList, int removeSize) {
+        int removeIndex = removeList.size() - removeSize;
+        for (int i = removeList.size() - 1; i >= removeIndex; i--) {
+            removeList.remove(i);
         }
+    }
+
+    // Внутри коллекции данные вперемешку, требуют красивого вывода
+    public void prettyPrint(List<WebElement> printList) {
+        final int weekSize = 7;
+        final int timeOfDaySize = 4;
+
+        for (int i = 0; i < weekSize; i++) {
+            System.out.printf("\n%s %s\n", printList.get(i).getText(), printList.get(i + weekSize).getText());
+
+            for (int j = 0; j < timeOfDaySize; j++) {
+                System.out.printf("%s\n", printList.get(i * timeOfDaySize + weekSize * 2 + j).getText()); // Выводим время суток и температуру (выглядит страшно, но вроде логичено)
+            }
+        }
+    }
+
+    // Берем температуру за неделю
+    public void getWeekTemp() {
+        final int extra = 10 - 7; // Берем температуру за 10 дней, а нужно за 7 - 3 лишние дни
+
+        List<WebElement> weather = webDriver.findElements(By.xpath(("//strong")));
+        removeExtra(weather, extra);
+        weather.addAll(webDriver.findElements(By.xpath("//small")));
+        removeExtra(weather, extra);
+        weather.addAll(webDriver.findElements(By.className("weather-table__body-cell_type_daypart")));
+        removeExtra(weather, extra * 4); // Умножаем количество дней на количество суток
+
+        prettyPrint(weather);
     }
 
     public void exit() {
